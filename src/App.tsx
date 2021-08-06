18c39718fbcea576fb4bef6e770f874afc371940
Gideon Crawley <gscrawley@gmail.com>
2021-08-06 05:10:59 -0700

    testing

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.scss'
+
+import WorldMapAtlas from './components/WorldMap/WorldMapAtlas'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <WorldMapAtlas />
+
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+
